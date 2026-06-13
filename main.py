f220dfe296b0ded2efa32825bdc1eca7480dee19
bhandkunal02-eng <bhand.kunal02@gmail.com>
2026-06-13 20:48:22 +0530

    Refactor file path for quotes in main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ password="skbr mpgc ixen uosy"
 now=dt.datetime.now()
 weekday=now.weekday()  
 if weekday== 5:
-    with open("E:\Pyhon Basic to Advance ,Udemy\Python-Day-32_email\Birthday+Wisher+(Day+32)+start\Birthday Wisher (Day 32) start\quotes.txt","r") as quote_file:
+    with open("quotes.txt","r") as quote_file:
         
         all_quotes=quote_file.readlines()
         quote=random.choice(all_quotes)
